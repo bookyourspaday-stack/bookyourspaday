1add7e59636b46fb4fa7804e3297f5f5ddc627f4
bookyourspaday-stack <bookyourspaday@gmail.com>
2025-08-28 15:37:55 -0400

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-10 text-center text-2xl">
-      <h1>Book Your Spa Day</h1>
-      <p>React + Vite + Tailwind setup working!</p>
-    </div>
-  )
-}
